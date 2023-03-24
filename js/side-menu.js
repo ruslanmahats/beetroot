1073047fbd658b0c53c4aaceacd3598ae7dba08d
Ruslan Mahats <ruslanmahats13@gmail.com>
2023-03-24 11:25:46 +0200

    bug fix

--- js/side-menu.js
+++ js/side-menu.js
@@ -5,26 +5,14 @@ const topBarElm = document.querySelector('.top-bar');
 const logoElm = document.querySelector('.logo');
 const logoIconElm = document.querySelector('.logo__icon');
 const logoTextElm = document.querySelector('.logo__text');
-const sideBarSectionDividerElm = document.querySelector('.side-bar__section-divider');
-const sideBarSectionNameElm = document.querySelector('.side-bar__section-name');
-const sideBarItemLinkElm = document.querySelector('.side-bar__item-link');
-const sideBarIconElm = document.querySelector('.side-bar__icon');
-const sideBarLabelElm = document.querySelector('.side-bar__label');
-const sideBarItemNameElm = document.querySelector('.side-bar__item-name');
 
 
 sideBarHideLinkElm.addEventListener('click', () => {
+	sideBarHideLinkElm.classList.toggle('side-bar__hide-link--active');
 	hideElm.classList.toggle('hide--active');
 	sideBarElm.classList.toggle('side-bar--active');
 	topBarElm.classList.toggle('top-bar--active');
 	logoElm.classList.toggle('logo--active');
 	logoIconElm.classList.toggle('logo__icon--active');
 	logoTextElm.classList.toggle('logo__text--active');
-	sideBarSectionDividerElm.classList.toggle('side-bar__section-divider--active');
-	sideBarSectionNameElm.classList.toggle('side-bar__section-name--active');
-	sideBarItemLinkElm.classList.toggle('side-bar__item-link--active');
-	sideBarIconElm.classList.toggle('side-bar__icon--active');
-	sideBarLabelElm.classList.toggle('side-bar__label--active');
-	sideBarItemNameElm.classList.toggle('side-bar__item-name--active');
-	sideBarHideLinkElm.classList.toggle('side-bar__hide-link--active');
 });
