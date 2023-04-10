b0fe3e517c520b3fe75adb86549b9b21aa540412
Ruslan Mahats <ruslanmahats13@gmail.com>
2023-04-10 11:14:17 +0300

    some tasks

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// let 1name;
+// let function;
+
+/*
+let let;
+let some-var;
+let variable;
+*/
+
+// let firstName;
+// let lastName;
+// let userFirstName;
+// let userLastname;
+
+// let CamelCase, lowerCamelCase, snake_case;
+
+// const curYear = 2023;
+// const userFirstName = prompt("Вкажіть ваше ім'я");
+// alert(`Привіт, ${userFirstName}!`);
+// const userBirthYear = prompt("Вкажіть ваш рік народження:");
+// const userAge = (curYear - Number(userBirthYear));
+// alert(`Шановний ${userFirstName}, ваш вік ${userAge} років.`);
+// const squareSide = prompt("Задайте сторону квадрата (см):")
+// const squarePerimeter = (Number(squareSide) * 4);
+// alert(`Периметр квадрата ${squarePerimeter}см, всього доброго, ${userFirstName}.`);
+
+// const circleRadius = prompt("Вкажіть радіус кола (см):");
+// const circleArea = (Math.PI * (Number(circleRadius) ** 2));
+// alert(`Радіус кола, ${circleArea}см!`);
+
+const travelDistance = prompt("Вкажіть відстань між містами (км):");
+const travelTime = prompt("Вкажіть час, за який ви б хотіли дістатись (год.):");
+const averageSpeed = (Number(travelDistance) / Number(travelTime));
+alert(`Ваша середня швидкість повинна бути не менше ніж ${averageSpeed}км/год, щоб прибути вчасно!`);
