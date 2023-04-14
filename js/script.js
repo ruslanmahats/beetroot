e86571c90f43524f3e8f33fcd414373fbafbcf65
Ruslan Mahats <ruslanmahats13@gmail.com>
2023-04-15 00:07:12 +0300

    some

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+// let factorial = (n) => {
+// 	let res = 1;
+// 	if (n != 0) {
+// 		for (let i = n; i > 0; i--) {
+// 			res = res * i;
+// 		}
+// 	}
+// 	return res;
+// }
+
+// console.log(factorial(7));
+
+
+let i = 1;
+let timer = setTimeout(function run() {
+	console.log(i);
+	setTimeout(run, 1000);
+}, 1000);
+
+// clearTimeout(timer);
+
+// setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
+
+// повторить с интервалом 2 секунды
+// let timerId = setTimeout(() => console.log('tick'), 1000);
+
+// остановить вывод через 5 секунд
+setTimeout(() => { clearTimeout(timer); console.log('stop'); }, 5000);
