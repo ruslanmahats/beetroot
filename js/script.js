42ff8a05013b586d00ef40375676c676f91450cf
Ruslan Mahats <ruslanmahats13@gmail.com>
2023-04-29 10:04:34 +0300

    some

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const edit = document.querySelector('.edit');
+
+document.addEventListener('keydown', (event) => {
+
+	if (event.key == 'Control' && event.key == 'Control') {
+		console.log(event);
+		event.preventDefault();
+	}
+
+});
